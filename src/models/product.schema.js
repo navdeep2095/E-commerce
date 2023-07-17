07e9c9cc05c17b81f227d011209a1941af12535e
navdeep2095 <40605788+navdeep2095@users.noreply.github.com>
2023-07-17 14:03:10 +0000

    Product schema added with relation to category

---
+++ src/models/product.schema.js
@@ -0,0 +1,49 @@
+import mongoose, { STATES } from 'mongoose';
+
+const productSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: [true, "Please provide this field"],
+            trim: true,
+            maxLength: [120, "Please provide name less than 120 characters"]
+        },
+        price: {
+            type: Number,
+            required: [true, "Please provide this field"],
+            trim: true,
+            maxLength: [5, "Please provide name less than 120 characters"]
+        },
+        description: {
+            type: String,
+            maxLength: [200, "Please provide description less than 200 characters"]
+        },
+        // For storing multiple photos in mongodb if user wants to upload only one photo then we can use string for it
+        // user is providing url where photos are uploaded from frontend
+        photos: [
+            {
+                secureUrl: {
+                    type: String,
+                    required: true
+                }
+            }
+        ],
+        stock: {
+            type: Number,
+            default: 0
+        },
+        sold: {
+            type: Number,
+            default: 0
+        },
+        // product belongs to particular category in collection schema so productSchema is refering to collectionSchema.
+        collectionId: {
+            ref: "Collection"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export default mongoose.models("Product",productSchema);
