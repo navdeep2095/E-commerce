62f21046ec82eb701c0d31204a401492b7126659
navdeep2095 <40605788+navdeep2095@users.noreply.github.com>
2023-07-17 18:29:11 +0000

    User logged in and out

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,12 +3,13 @@ import CustomError from '../utils/CustomError.js';
 import User from '../models/user.schema.js';
 
 export const cookieOptions = {
+    // cookie will expire in 3 days
     expires: new Date(Date.now() + 3*24*60*60*1000),
     httpOnly: true
 }
 
-const signup = asyncHandler(async(req, res, next) => { 
-    const {username, password, email} = req.body();
+export const signup = asyncHandler(async (req, res) => { 
+    const {username, password, email} = req.body;
 
     // Validation
     // custom error class to handle the errors
@@ -41,5 +42,50 @@ const signup = asyncHandler(async(req, res, next) => {
         success: true,
         user: newUser,
         token
+    });
+});
+
+export const login = asyncHandler(async (req, res) => {
+    const {username, password} = req.body;
+
+    if(!username || !password) {
+        throw new CustomError("Please fill all the credentials", 400);
+    }
+
+    const loggedUser = User.findOne({email}).select("+password");
+
+    if(!loggedUser) {
+        throw new CustomError("Please check the credentials", 400);
+    }
+
+    const isPasswordCorrect = await loggedUser.compare(password);
+
+    if(isPasswordCorrect) {
+        const token = loggedUser.getJWTToken();
+
+        // password as undefined as mongoose will return password as well during creation of the user, from the second time
+        //  onwards it will not
+        loggedUser.password = undefined;
+
+        res.cookie("token", token, cookieOptions);
+        return res.status(200).json({
+            success: true,
+            token,
+            user:loggedUser
+        });
+    }
+
+    throw new CustomError("Password is incorrect", 400);
+});
+
+export const logout = asyncHandler( async () => {
+    res.cookie("token", null, {
+       expires: new Date(Date.now()),
+       httpOnly: true 
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "User Logged Out"
     });
 });
