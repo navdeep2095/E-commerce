60622e0ac267bb847661bf8912bdbaca501f594c
navdeep2095 <40605788+navdeep2095@users.noreply.github.com>
2023-07-17 13:14:20 +0000

    Forgot password functionality

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,9 @@ dotenv.config();
 
 const config = {
     PORT: process.env.PORT || null,
-    MONGOURL: process.env.MONGO_URL || null
+    MONGOURL: process.env.MONGO_URL || null,
+    JWT_SECRET: process.env.JWT_SECRET || null,
+    JWT_EXPIRY: process.env.JWT_EXPIRY || null
 };
 
 export default config;
